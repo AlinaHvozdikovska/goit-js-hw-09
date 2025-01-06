a3a718a517e8f919b0fe08f8d8fd490c2f89b1c7
AliceStorm <Alice2646777@gmail.com>
2025-01-06 23:52:12 +0100

    deleted readme.pl, ro, tr. modified package.json, fix 1-gallery.hml, 1-gallery.js, 2-form.html, 2-form.js, styles.css

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,51 @@
-console.log('Form');
+const formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+
+/* проверка локального хранилища при загрузке стр */
+
+const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+if (savedData) {
+  formData.email = savedData.email;
+  formData.message = savedData.message;
+  form.email.value = formData.email;
+  form.message.value = formData.message;
+}
+
+/* Метод делегирования для обработки действия input */
+form.addEventListener('input', event => {
+  const { name, value } = event.target;
+  if (name === 'email') {
+    formData.email = value;
+  } else if (name === 'message') {
+    formData.message = value;
+  }
+
+  /* Запись данных в локальное хранилище */
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+/* Обработка отправки формы */
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  /* проверка на заполнение полей */
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+  } else {
+    console.log(formData);
+
+    /* Очистка локального хранилища и formData */
+
+    localStorage.removeItem('feedback-form-state');
+    formData.email = '';
+    formData.message = '';
+
+    /* очистка полей ввода */
+    form.email.value = '';
+    form.message.value = '';
+  }
+});
